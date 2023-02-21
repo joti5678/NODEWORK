fb7dbf4e7cf8c5dee5c2feeef187f8685d2a88ba
joti5678 <joti5678@gmail.com>
2023-02-21 13:01:43 +0600

    2nd commit

--- LESSON3/index.js
+++ LESSON3/index.js
@@ -1,6 +1,13 @@
-const os = require('os');
-console.log(os.hostname);
-console.log(os.userInfo);
-console.log(os.homedir);
-console.log(os.totalmem());
-console.log(os.freemem());
+const {totalmem, freemem} = require('os');
+const path = require('path');
+
+console.log(totalmem());
+console.log(freemem());
+console.log("Directory Name:" + __dirname);
+console.log("File Name:" + __filename);
+
+
+const extenname = path.extname(  __filename)
+const joinname = path.join( __dirname + "/Views/newfile" +  path.extname(  __filename))
+console.log("extenname Name:" +extenname);
+console.log("joinname Name:" + joinname);
